adc6a1958d6f83f162a9dcfec9a84a0c7f1accdc
ideask <9982733@qq.com>
2019-10-08 23:11:46 +0800

    add dataProcess function, change the images file path, rewrite label txt path

---
+++ data/dataset.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 -*-
+import os
+from PIL import Image
+from torch.utils import data
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+import matplotlib.pyplot as plt
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+class Hand(data.Dataset):
+    
+    def __init__(self, root, transforms=None, train=True):
+        '''
+        Get images, divide into train/val set
+        '''
+
+        self.train = train
+        self.images_root = root
+
+        self._read_txt_file()
+
+        print(" os.getcwd()", os.getcwd())
+        if transforms is None:
+	    # mean(R,G,B) std(R,G,B),Normalized_image=(image-mean)/std
+            normalize = T.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+
+            if not train: 
+                self.transforms = T.Compose([
+                    T.Resize(224),
+                    T.CenterCrop(224),
+                    T.ToTensor(),
+                    normalize
+                    ]) 
+            else:
+                self.transforms = T.Compose([
+                    T.Resize(256),
+                    T.RandomResizedCrop(224),
+                    T.RandomHorizontalFlip(),
+                    T.ToTensor(),
+                    normalize
+                    ])
+                
+    def _read_txt_file(self):
+        '''
+        Get images_path, images_labels from txt_file
+        '''
+        self.images_path = []
+        self.images_labels = []
+
+        if self.train:
+            txt_file = self.images_root + "/images/train.txt"
+        else:
+            txt_file = self.images_root + "/images/test.txt"
+
+        with open(txt_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                item = line.strip().split(' ')
+                self.images_path.append(item[0])
+                self.images_labels.append(item[1])
+
+    def __getitem__(self, index):
+        '''
+        return the data of one image
+        '''
+        img_path = self.images_root+self.images_path[index]
+        label = self.images_labels[index]
+        data = Image.open(img_path)
+        data = self.transforms(data)
+        return data, int(label)
+    
+    def __len__(self):
+        '''
+        return the num of images
+        '''
+        return len(self.images_path)
+
+if __name__ == '__main__':
+    data_root = os.getcwd()
+    train_data = Hand(data_root, transforms=None, train=True)
+    batch_size = 5
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=0)
+    for images, labels in train_dataloader:
+        plt.figure(figsize = (20,5))
+        num = 0
+        for image, label in zip(images, labels):
+            image = image.numpy()
+            image = image.transpose((1, 2, 0))
+            num += 1
+            plt.subplot(1,batch_size,num)
+            plt.imshow(image)
+            plt.title('Prediction number:  {}'.format(label))
+        plt.show()
